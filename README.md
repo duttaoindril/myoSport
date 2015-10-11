3a9f35649da36a0a2461d9da8bef4fa9dd8b6a49
duttaoindril <duttaoindril@gmail.com>
2015-10-11 01:34:01 -0700

    Starting from scratch with an express node server

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # myoSport
+
+Get Jquery or simply install locally
+<script src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
+
+npm install to get all the required node moduls
---
+++ src/app.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var express = require("express");
+var dtw = require("dtw");
+var firebase = require("firebase");
+var myo = require("myo");
+var jade = require("jade");
+
+var app = express();
+app.listen(3000);
