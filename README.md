54c054bc7d13082c8ae02f46b5694766b3b0d328
duttaoindril <duttaoindril@gmail.com>
2015-10-11 02:51:11 -0700

    Adding in firebase and express and basic cool stuff

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # myoSport
 
-Get Jquery or simply install locally
-<script src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
+Get Jquery from http://code.jquery.com/jquery-2.1.4.min.js or simply install locally!
 
-npm install to get all the required node moduls
+npm install to get all the required node-modules
--- src/app.js
+++ src/app.js
@@ -1,10 +1,29 @@
 "use strict";
+var port = 3000;
 
 var express = require("express");
 var dtw = require("dtw");
-var firebase = require("firebase");
+var Firebase = require("firebase");
 var myo = require("myo");
 var jade = require("jade");
 
 var app = express();
-app.listen(3000);
+
+app.get('/', function(req, res) {
+    res.send("<h1>I absolutely love Treehouse!<h1>");
+});
+
+app.get('/dtwtest', function(req, res) {
+    res.send("<h1>I absolutely love Treehouse!<h1>");
+});
+
+app.get('/fbdump', function(req, res) {
+    var myoSport = new Firebase("https://myosport.firebaseio.com/");
+    myoSport.child("duttaoindril").on("value", function(data) {
+        res.send(data.val());
+    });
+});
+
+app.listen(port, function() {
+    console.log("The frontend server is running at:\nlocalhost:"+port);
+});
